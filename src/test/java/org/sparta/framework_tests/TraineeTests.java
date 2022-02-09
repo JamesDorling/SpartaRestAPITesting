6266e20ef8894f22e5effb256182876a7db08978
JamesDorling <jamesrdorling@gmail.com>
2022-02-09 14:31:11 +0000

    Made more crud operation tests to test putting and getting

--- src/test/java/org/sparta/framework_tests/TraineeTests.java
+++ src/test/java/org/sparta/framework_tests/TraineeTests.java
@@ -14,17 +14,18 @@ import java.util.List;
 import static org.sparta.framework.connection.ConnectionManager.*;
 import static org.sparta.framework.Injector.*;
 
+@TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
 public class TraineeTests {
-    private List<TraineeDTO> traineeList;
-    private final boolean editDatabase = true; //Temporary boolean so I do not add an employee called "dames" every time I run tests
+    private TraineeDTO trainee;
+    private final boolean editDatabase = false; //Temporary boolean so I do not add an employee called "dames" every time I run tests
 
     @BeforeEach
     void init() {
-        TraineeDTOList traineeDTOList = (TraineeDTOList) injectDTO(ConnectionManager.makeUrl().spartan().link(), DTOEnum.TRAINEE_LIST);
-        traineeList = traineeDTOList.getEmbedded().getSpartanEntityList();
+        trainee = (TraineeDTO) injectDTO(ConnectionManager.makeUrl().getSpecificSpartan("6203af8f51765e2883463d96"), DTOEnum.TRAINEE);
     }
 
     @Nested
+    @Order(value = 3)
     @DisplayName("TraineeDTO Tests")
     class TraineeDTOTests {
         @Test
@@ -35,104 +36,112 @@ public class TraineeTests {
 
         @Test
         @DisplayName("Name is Correct Test")
-        void fullNameTest() {Assertions.assertEquals("Macias Monroe", traineeList.get(0).getFullName());}
+        void fullNameTest() {Assertions.assertEquals("james dorling", trainee.getFullName());}
 
         @Test
         @DisplayName("StartDate as Date Returns Correct Date Test")
         void startDateAsDateTest() {
-            Assertions.assertEquals(LocalDate.of(2022, 2, 8), traineeList.get(0).getStartDateAsDate());
+            Assertions.assertEquals(LocalDate.of(2023, 1, 1), trainee.getStartDateAsDate());
         }
 
         @Test
         @DisplayName("EndDate as Date Returns Correct Date Test")
         void endDateAsDateTest() {
-            Assertions.assertEquals(LocalDate.of(2022, 3, 15), traineeList.get(0).getEndDateAsDate());
+            Assertions.assertEquals(LocalDate.of(2023, 2, 26), trainee.getEndDateAsDate());
         }
 
         @Test
         @DisplayName("First Name is Not Null test")
-        void firstNameNotNull() {Assertions.assertTrue(traineeList.get(0).firstNameIsNotNull());}
+        void firstNameNotNull() {Assertions.assertTrue(trainee.firstNameIsNotNull());}
 
         @Test
         @DisplayName("Last Name is Not Null test")
-        void lastNameNotNull() {Assertions.assertTrue(traineeList.get(0).lastNameIsNotNull());}
+        void lastNameNotNull() {Assertions.assertTrue(trainee.lastNameIsNotNull());}
 
         @Test
         @DisplayName("Start Date is Not Null test")
-        void startDateNotNull() {Assertions.assertTrue(traineeList.get(0).startDateIsNotNull());}
+        void startDateNotNull() {Assertions.assertTrue(trainee.startDateIsNotNull());}
 
         @Test
         @DisplayName("end Date is Not Null test")
-        void endDateNotNull() {Assertions.assertTrue(traineeList.get(0).endDateIsNotNull());}
+        void endDateNotNull() {Assertions.assertTrue(trainee.endDateIsNotNull());}
 
         @Test
         @DisplayName("ID is Not Null test")
-        void idNotNull() {Assertions.assertTrue(traineeList.get(0).idIsNotNull());}
+        void idNotNull() {Assertions.assertTrue(trainee.idIsNotNull());}
 
         @Test
         @DisplayName("Course ID is Not Null test")
-        void courseIdNotNull() {Assertions.assertTrue(traineeList.get(0).courseIdIsNotNull());}
+        void courseIdNotNull() {Assertions.assertTrue(trainee.courseIdIsNotNull());}
 
         @Test
         @DisplayName("No Data is Null")
-        void noDataIsNull() {Assertions.assertTrue(traineeList.get(0).noDataIsNull());}
+        void noDataIsNull() {Assertions.assertTrue(trainee.noDataIsNull());}
 
         @Test
         @DisplayName("Start Date is before End Date")
         void startIsBeforeEnd() {
-            Assertions.assertTrue(traineeList.get(0).startIsBeforeEnd());
+            Assertions.assertTrue(trainee.startIsBeforeEnd());
         }
 
         @Test
         @DisplayName("End Date is after Start Date") //Redundant test, but if one fails and one passes something is up
         void endIsAfterStart() {
-            Assertions.assertTrue(traineeList.get(0).endIsAfterStart());
+            Assertions.assertTrue(trainee.endIsAfterStart());
         }
 
         @Test
         @DisplayName("Start Date is after 0")
-        void startIsAfterTheCalendarStarted() {Assertions.assertTrue(traineeList.get(0).startIsAfter(LocalDate.of(2022, 1, 1)));}
+        void startIsAfterTheCalendarStarted() {Assertions.assertTrue(trainee.startIsAfter(LocalDate.of(2022, 1, 1)));}
 
         @Test
         @DisplayName("End Date is before the set max date")
-        void endIsBeforeTheEndOfTime() {Assertions.assertTrue(traineeList.get(0).endIsBefore(LocalDate.of(2050, 12, 31)));}
+        void endIsBeforeTheEndOfTime() {Assertions.assertTrue(trainee.endIsBefore(LocalDate.of(2050, 12, 31)));}
 
         @Test
         @DisplayName("Course Name is correct")
-        void courseNameIsCorrect() {Assertions.assertEquals("business", traineeList.get(0).getCourseName());}
+        void courseNameIsCorrect() {Assertions.assertEquals("c#", trainee.getCourseName());} //Im not on this course but still
     }
 
     @Nested
+    @Order(value = 2)
     @DisplayName("TraineePOJO Tests")
     class TraineePojoTests {
         @Test
         @DisplayName("First Name is Correct Test")
-        void firstNameTest() {Assertions.assertEquals("Hendrix", traineeList.get(0).getFirstName());}
+        void firstNameTest() {Assertions.assertEquals("james", trainee.getFirstName());}
 
         @Test
         @DisplayName("Last Name is Correct Test")
-        void lastNameTest() {Assertions.assertEquals("Gardner", traineeList.get(0).getLastName());}
+        void lastNameTest() {Assertions.assertEquals("dorling", trainee.getLastName());}
 
         @Test
         @DisplayName("Course Start Date is Correct")
-        void courseStartIsCorrect() {Assertions.assertEquals("2022-02-08", traineeList.get(0).getCourseStartDate());}
+        void courseStartIsCorrect() {Assertions.assertEquals("2023-01-01", trainee.getCourseStartDate());}
 
         @Test
         @DisplayName("Course End Date is Correct")
-        void courseEndDateIsCorrect() {Assertions.assertEquals("2022-04-15", traineeList.get(0).getCourseEndDate());}
+        void courseEndDateIsCorrect() {Assertions.assertEquals("2023-02-26", trainee.getCourseEndDate());}
 
         @Test
         @DisplayName("Course ID is Correct")
-        void courseIdIsCorrectTest() {Assertions.assertEquals(3, traineeList.get(0).getCourseId());}
+        void courseIdIsCorrectTest() {Assertions.assertEquals(2, trainee.getCourseId());}
 
         @Test
         @DisplayName("Trainee ID is Correct")
-        void traineeIdIsCorrectTest() {Assertions.assertEquals("6203a427147baa0d50338b6f", traineeList.get(0).getId());}
+        void traineeIdIsCorrectTest() {Assertions.assertEquals("6203af8f51765e2883463d96", trainee.getId());}
     }
 
     @Nested
+    @Order(value = 1)
     @DisplayName("Crud Operation Tests")
     class CrudOperationTests {
+        @Test
+        @DisplayName("Get Trainee By ID")
+        void getTraineeById() {
+            Assertions.assertNotNull(trainee);
+        }
+        
         @Test
         @DisplayName("Posting a trainee")
         void postingATraineeTest() {
@@ -142,8 +151,8 @@ public class TraineeTests {
             System.out.println(makeUrl().spartan().getSpartanWithKey());
 
             TraineeDTOList traineeDTOList = (TraineeDTOList) injectDTO(ConnectionManager.makeUrl().spartan().link(), DTOEnum.TRAINEE_LIST);
-            traineeList = traineeDTOList.getEmbedded().getSpartanEntityList();
-            Assertions.assertEquals("dames", traineeList.get(traineeList.size()-1).getFirstName());
+            Assertions.assertEquals("dames", traineeDTOList.getEmbedded().getSpartanEntityList().get(
+                    traineeDTOList.getEmbedded().getSpartanEntityList().size()-1).getFirstName());
         }
 
         @Test
@@ -153,17 +162,44 @@ public class TraineeTests {
             UpdateTraineeForm updateTrainee = new UpdateTraineeForm("6203af8f51765e2883463d96","james", "dorling", 2, "2023-01-01");
             sendTraineePutRequest(updateTrainee.getJson(), makeUrl().getSpartanWithKey());
 
-            TraineeDTOList traineeDTOList = (TraineeDTOList) injectDTO(ConnectionManager.makeUrl().spartan().link(), DTOEnum.TRAINEE_LIST);
-            traineeList = traineeDTOList.getEmbedded().getSpartanEntityList();
-            Assertions.assertEquals(2, traineeList.get(traineeList.size()-1).getCourseId());
+            TraineeDTO editedTrainee = (TraineeDTO) injectDTO(ConnectionManager.makeUrl().getSpecificSpartan("6203af8f51765e2883463d96"), DTOEnum.TRAINEE);
+            Assertions.assertEquals(2, editedTrainee.getCourseId());
         }
 
         @Test
-        @DisplayName("Searching for a spartan by name")
-        void searchingForASpartanByNameTest() {
+        @DisplayName("Searching for a spartan by first name")
+        void searchingForASpartanByFirstNameTest() {
             TraineeDTOList traineeDTOList = (TraineeDTOList) injectDTO(ConnectionManager.makeUrl().spartan().firstName("james").link(), DTOEnum.TRAINEE_LIST);
             Assertions.assertEquals(traineeDTOList.getEmbedded().getSpartanEntityList().get(0).getFirstName(), "james");
         }
+
+        @Test
+        @DisplayName("Searching for a spartan by last name")
+        void searchingForASpartanByLastName() {
+            TraineeDTOList traineeDTOList = (TraineeDTOList) injectDTO(ConnectionManager.makeUrl().spartan().lastName("james").link(), DTOEnum.TRAINEE_LIST);
+            Assertions.assertEquals(traineeDTOList.getEmbedded().getSpartanEntityList().get(0).getLastName(), "dorling");
+        }
+
+        @Test
+        @DisplayName("Searching for a spartan by date before")
+        void searchingForASpartanByDateBefore() {
+            TraineeDTOList traineeDTOList = (TraineeDTOList) injectDTO(ConnectionManager.makeUrl().spartan()
+                    .BeforeAfter(UrlBuilder.TimeParameters.BEFORE).StartEnd(UrlBuilder.TimeParameters.START).date("2025-01-01").link(), DTOEnum.TRAINEE_LIST);
+            Assertions.assertTrue(traineeDTOList.getEmbedded().getSpartanEntityList().get(0).getStartDateAsDate()
+                    .isBefore(LocalDate.of(2025, 1,1)));
+        }
+
+        @Test
+        @DisplayName("Searching for a spartan by date after")
+        void searchingForASpartanByDateAfter() {
+            TraineeDTOList traineeDTOList = (TraineeDTOList) injectDTO(ConnectionManager.makeUrl().spartan()
+                    .BeforeAfter(UrlBuilder.TimeParameters.AFTER).StartEnd(UrlBuilder.TimeParameters.END).date("2021-01-01").link(), DTOEnum.TRAINEE_LIST);
+            Assertions.assertTrue(traineeDTOList.getEmbedded().getSpartanEntityList().get(0).getEndDateAsDate()
+                    .isAfter(LocalDate.of(2021, 1,1)));
+        }
+
     }
+
+
 
 }
